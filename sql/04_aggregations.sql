ddae82039aed5e9382f312d7a47ca0b0c881d857
Fode Manca <fomanca@c1r15s2.42porto.com>
2026-05-11 14:32:54 +0100

    Add PostgreSQL aggregate queries

--- sql/04_aggregations.sql
+++ sql/04_aggregations.sql
@@ -1,1 +1,15 @@
-SELECT COUNT(*) FROM titles;
+SELECT COUNT(*) FROM books;
+
+SELECT
+	authors.name AS author_name,
+	COUNT(books.id) AS total_books -- conta qnts liros existem por autor
+FROM authors
+JOIN books
+	ON authors.id = books.author_id
+GROUP BY authors.name;
+
+SELECT MIN(year_of_publication)
+FROM books;
+
+SELECT MAX(year_of_publication)
+FROM books;
