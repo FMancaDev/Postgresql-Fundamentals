62041b1e37b23f1bec909f544e6a9afb60434ce0
Fode Manca <fomanca@c1r14s5.42porto.com>
2026-05-14 13:24:10 +0100

    updating files

--- sql/05_update_delete.sql
+++ sql/05_update_delete.sql
@@ -2,7 +2,8 @@ UPDATE books
 SET year_of_publication = 1949
 WHERE title = 'Penguin Essentials';
 
-DELETE FROM books;
+DELETE FROM books
+WHERE author_id = (SELECT id FROM authors WHERE name = 'Fernando Pessoa');
 
 DELETE FROM authors
 WHERE name = 'Fernando Pessoa';
